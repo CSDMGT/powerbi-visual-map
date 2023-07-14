31bc443133155ce92ca578d6a6687f5870efedf6
zhangWenFa <18831816834@163.com>
2023-07-14 09:53:09 +0800

    修改路径

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/csd-blog/',
+  base: '/powerbi-visual-map/',
   title: "My Awesome Project",
   description: "A VitePress Site",
   themeConfig: {
